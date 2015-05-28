b3c033213e4ea82d21d96a7803882936e384274f
Alex Haslehurst <alex.haslehurst@gmail.com>
2015-05-28 13:16:58 +0100

    Removed reset

--- ArSwitch.ino
+++ ArSwitch.ino
@@ -2,10 +2,6 @@
 // HIGH = Power off, LOW = Power on.
 const int powerSwitchPin = 2;
 
-// Connected to reset push button with upll-up resistor.
-// HIGH = Do nothing, LOW = Reset.
-const int resetSwitchPin = 3;
-
 // Connected to MOSFET with pull-down resistor.
 // LOW = Pi off, HIGH = Pi on.
 const int powerSignalPin = 9;
@@ -19,82 +15,68 @@ const int rPiGpioOutPin = 10;
 const int rPiGpioInPin = 11;
 
 void setup() {
-  pinMode(powerSwitchPin, INPUT_PULLUP);
-  pinMode(resetSwitchPin, INPUT_PULLUP);
-  pinMode(powerSignalPin, OUTPUT);
-  pinMode(rPiGpioOutPin, OUTPUT);
-  pinMode(rPiGpioInPin, INPUT);
+    pinMode(powerSwitchPin, INPUT_PULLUP);
+    pinMode(powerSignalPin, OUTPUT);
+    pinMode(rPiGpioOutPin, OUTPUT);
+    pinMode(rPiGpioInPin, INPUT);
   
-  // Set values.
-  digitalWrite(powerSignalPin, LOW);
-  digitalWrite(rPiGpioOutPin, LOW);
+    // Set values.
+    digitalWrite(powerSignalPin, LOW);
+    digitalWrite(rPiGpioOutPin, LOW);
 }
 
-void loop() {
-  // Check if power switch has been toggled.
-  int currentPowerSwitchState = digitalRead(powerSwitchPin);
-  int currentResetState = digitalRead(resetSwitchPin);
-  int currentPiPowerState = digitalRead(rPiGpioInPin);
+void loop() {    
+    // Check if power switch has been toggled.
+    int currentPowerSwitchState = digitalRead(powerSwitchPin);
+    int currentPiPowerState = digitalRead(rPiGpioInPin);
   
-  // This equality looks weird because we're comparing a pulled up value with one from pi.
-  if(currentPiPowerState == currentPowerSwitchState) {
-    if(currentPowerSwitchState == LOW) {
-      switchOn();
-    } else {
-      switchOff();
-    }
+    // This equality looks weird because we're comparing a pulled up value with one from pi.
+    if(currentPiPowerState == currentPowerSwitchState) {
+        if(currentPowerSwitchState == LOW) {
+            switchOn();
+        } else {
+            switchOff();
+        }
     
-    return;
-  }
-  
-  // Check if Pi is off. If so then nothing to do.
-  if(currentPiPowerState == LOW) {
-    return;
-  }
-  
-  // Check reset switch.
-  if(currentResetState == LOW) {
-    switchOff();
-    switchOn();
-    return;
-  }
+        return;
+    }
 }
 
 void switchOn() {
-  // Turn on the MOSFET.
-  digitalWrite(powerSignalPin, HIGH);
+    // Turn on the MOSFET.
+    digitalWrite(powerSignalPin, HIGH);
   
-  // Set the gpio out LOW as Pi will halt if it sees HIGH.
-  digitalWrite(rPiGpioOutPin, LOW);
+    // Set the gpio out LOW as Pi will halt if it sees HIGH.
+    digitalWrite(rPiGpioOutPin, LOW);
   
-  // Wait for signal from Pi.
-  while(1) {
-    int currentGpioInState = digitalRead(rPiGpioInPin);
+    // Wait for signal from Pi.
+    while(1) {
+        int currentGpioInState = digitalRead(rPiGpioInPin);
     
-    if(currentGpioInState == LOW) {
-      return;
+        if(currentGpioInState == LOW) {
+            return;
+        }
     }
-  }
 }
 
 void switchOff() {
-  // Trigger Pi halt.
-  digitalWrite(rPiGpioOutPin, HIGH);
+    // Trigger Pi halt.
+    digitalWrite(rPiGpioOutPin, HIGH);
   
-  // Wait for signal to drop from Pi.
-  while(1) {
-    int currentGpioInState = digitalRead(rPiGpioInPin);
+    // Wait for signal to drop from Pi.
+    while(1) {
+        int currentGpioInState = digitalRead(rPiGpioInPin);
     
-    if(currentGpioInState == HIGH) {
-      // Give it 5 more seconds.
-      delay(5000);
+        if(currentGpioInState == HIGH) {
+            // Give it 5 more seconds.
+            delay(5000);
       
-      // Turn off the MOSFET and Pi halt signal.
-      digitalWrite(powerSignalPin, LOW);
-      digitalWrite(rPiGpioOutPin, LOW);
-      return;
+            // Turn off the MOSFET and Pi halt signal.
+            digitalWrite(powerSignalPin, LOW);
+            digitalWrite(rPiGpioOutPin, LOW);
+            return;
+        }
     }
-  }
 }
 
 
